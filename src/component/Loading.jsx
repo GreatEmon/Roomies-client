e3841cfb69767b4bfa87eb918b116cafd5cf241e
GreatEmon <contact.emonpro@gmail.com>
2025-09-23 00:13:17 +0600

    edited-version

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Loading = () => {
+  
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
         <span className="loading loading-bars loading-lg"></span>
