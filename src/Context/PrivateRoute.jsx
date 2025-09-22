45a52bf8c84ae618e3bae1feead1d71cdce5b668
GreatEmon <contact.emonpro@gmail.com>
2025-09-23 00:16:04 +0600

    edited-version

--- src/Context/PrivateRoute.jsx
+++ src/Context/PrivateRoute.jsx
@@ -6,9 +6,11 @@ import Loading from '../component/Loading'
 const PrivateRoute = ({children}) => {
   const {user,loading} = use(AuthContext)
   const location = useLocation()
+
   if(loading){
     return <Loading></Loading>
   }
+  
   if(user && user?.email){
     return children
   }else{
