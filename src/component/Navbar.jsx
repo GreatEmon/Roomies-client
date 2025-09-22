d6ec5d23a2529e243df5d8f91dee8b8458287dda
GreatEmon <contact.emonpro@gmail.com>
2025-09-23 00:15:12 +0600

    edited-version

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router'
 import { AuthContext } from '../Context/AuthProvider'
 
 const Navbar = () => {
+
     const { user } = use(AuthContext)
 
     const menu = <div className='md:flex gap-4'>
@@ -12,6 +13,7 @@ const Navbar = () => {
         <li><NavLink to="browse" className="px-3 py-2">Browse Listing</NavLink></li>
         <li><NavLink to="mylist" className="px-3 py-2">My Listings</NavLink></li>
     </div>
+
     return (
         <div className="navbar bg-base-100 container mx-auto">
             <div className="navbar-start">
@@ -32,6 +34,7 @@ const Navbar = () => {
                     {menu}
                 </ul>
             </div>
+
             <div className="navbar-end space-x-2">
                 <label className="toggle text-base-content text-xl mr-4">
                     <input type="checkbox" value="synthwave" className="theme-controller" />
@@ -47,9 +50,8 @@ const Navbar = () => {
                         />
                     </div>
                 </div>}
+                
                 {!user ? <Link to="/register" className='btn btn-primary'>Signup</Link> : <Link to="/logout" className='btn btn-primary'>Signout</Link>}
-
-
             </div>
         </div>
     )
