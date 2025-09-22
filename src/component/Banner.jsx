adaf9e4942c3727c914b8e10446d3875518c050c
GreatEmon <contact.emonpro@gmail.com>
2025-09-23 00:02:58 +0600

    edited

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -29,11 +29,14 @@ export const Banner = () => {
                             </div>
                         </div>
                     </div>
+
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
+
+
                 <div id="slide2" className="carousel-item relative w-full">
                     <div
                         className="hero min-h-[650px]"
@@ -63,6 +66,8 @@ export const Banner = () => {
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
+
+                
                 <div id="slide3" className="carousel-item relative w-full">
                     <div
                         className="hero min-h-[650px]"
